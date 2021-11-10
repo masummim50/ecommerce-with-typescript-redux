34d8cd23e554a78f6466ac2d9e15c20bba70a3ad
unknown <masummim50@gmail.com>
2021-11-10 18:50:33 +0600

    first commit

--- src/components/common/productCard/ProductCard.tsx
+++ src/components/common/productCard/ProductCard.tsx
@@ -8,10 +8,11 @@ const ProductCard = (props: { product: IProduct }) => {
   const {_id, name, img, price, star, starCount } = props.product;
   return (
     <Link to={`/productdetails/${_id}`} className="col-md-3 mb-4">
-      <Card className="shadow">
-      <Card.Img className="p-4" src={img}/>
-      <Card.Body>
+      <Card className="shadow h-100">
+      <Card.Img className="p-4 border-bottom" src={img}/>
+      <Card.Body className="d-flex flex-column justify-content-between">
         <Card.Text>{name.slice(0,30)}...</Card.Text>
+        <div className="price-info">
         <Card.Text>${price}</Card.Text>
         {
           new Array(star).fill('').map((st, index)=> <AiFillStar key={index}/>)
@@ -20,6 +21,7 @@ const ProductCard = (props: { product: IProduct }) => {
           Array(5-star).fill('').map((st, index)=> <AiOutlineStar key={index}/>)
         }
         <span>({starCount})</span>
+        </div>
       </Card.Body>
     </Card>
     </Link>
--- src/redux/cartReducer.ts
+++ src/redux/cartReducer.ts
@@ -6,12 +6,17 @@ import { cartActions } from "./actions/actionTypes";
 export const cartReducer =(state=[] as IProduct[], action:actionTypes)=> {
   switch(action.type){
     case cartActions.ADD_TO_CART:{
-      if(action.payload.quantity === undefined){
-        const modified = {...action.payload, quantity:1}
-        return [...state, modified]
-      }else{
-        return []
-      }
+      console.log('consoling state', state)
+        if((state.filter(item=> item.key === action.payload.key)).length === 1){
+          const newstate = state.filter(item=> item.key !== action.payload.key)
+          const prev = state.filter(item=> item.key === action.payload.key)[0].quantity;
+          const modified = {...action.payload, quantity:prev as number + 1
+          }
+          return [...newstate, modified]
+        }else{
+          const newobject = {...action.payload, quantity: 1}
+          return [...state, newobject]
+        }
     }
     case cartActions.REMOVE_FROM_CART:{
       const newstate = state.filter(item=> item.key !== action.payload)
